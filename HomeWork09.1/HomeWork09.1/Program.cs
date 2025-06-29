d46ee1b5aeff2da66d9e5e7006c37c4907fa0e5a
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-06-29 12:54:50 +0300

    Resolved task 1

--- HomeWork09.1/HomeWork09.1/Program.cs
+++ HomeWork09.1/HomeWork09.1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написати програму, що знаходить другий найбільший елемент масиву.
+const int LENGTH = 10;
+const int MIN = -50;
+const int MAX = 151;
+
+int[] arr = new int[LENGTH];
+int maxNumber = int.MinValue;
+int secondMxNumber = int.MinValue;
+
+var random = new Random();
+
+for (int i = 0; i < LENGTH; i++)
+{
+	arr[i] = random.Next(MIN, MAX);
+	Console.Write(arr[i] + "\t");
+}
+
+for (int i = 0; i < LENGTH; i++)
+	if (arr[i] > maxNumber)
+	{
+		secondMxNumber = maxNumber;
+		maxNumber = arr[i];
+	}
+	else if (arr[i] > secondMxNumber && arr[i] != maxNumber)
+		secondMxNumber = arr[i];
+
+Console.WriteLine($"\n\nMax number: {maxNumber}\n\nSecond max number: {secondMxNumber}");
