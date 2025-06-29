c94ae80cd82c3f75cc5990da7545c4e35eed0e11
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-06-29 19:36:09 +0300

    Some solution, not final

--- HomeWork09.1/HomeWork09.2/Program.cs
+++ HomeWork09.1/HomeWork09.2/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написати програму, що буде сортувати за зростанням елементи двовимірного масиву.
+const int ROW = 3;
+const int COLUMN = 3;
+const int MIN = -20;
+const int MAX = 20;
+
+int[,] arr = new int[ROW, COLUMN]
+	{
+		{45,2,33 },
+		{5,-85,60 },
+		{1234,35,-8}
+	};
+
+int[] row = new int[COLUMN];
+int temp = 0;
+for (int i = 0; i < ROW; i++)
+{
+	for (int j = 0; j < COLUMN; j++)
+	{
+		row[j] = arr[i, j];
+	}
+
+	for (int l = 0; l < COLUMN - 1; l++)
+		for (int k = 0; k < COLUMN - 1 - l; k++)
+		{
+			if (row[k] > row[k + 1])
+			{
+				temp = row[k];
+				row[k] = row[k + 1];
+				row[k + 1] = temp;
+			}
+		}
+
+	for (int j = 0; j < COLUMN; j++)
+	{
+		arr[i, j] = row[j];
+	}
+}
+
+
+for (int i = 0; i < ROW; i++)
+	for (int j = 0; j < COLUMN; j++)
+		Console.WriteLine(arr[i, j] + "\t\n");
