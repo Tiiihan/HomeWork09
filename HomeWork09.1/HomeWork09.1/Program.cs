95c04951462957107fe008757cc397147246f20b
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-07-02 23:04:34 +0300

    Fixed variable name

--- HomeWork09.1/HomeWork09.1/Program.cs
+++ HomeWork09.1/HomeWork09.1/Program.cs
@@ -1,11 +1,12 @@
 ﻿//Написати програму, що знаходить другий найбільший елемент масиву.
+
 const int LENGTH = 10;
 const int MIN = -50;
 const int MAX = 151;
 
 int[] arr = new int[LENGTH];
 int maxNumber = int.MinValue;
-int secondMxNumber = int.MinValue;
+int secondMaxNumber = int.MinValue;
 
 var random = new Random();
 
@@ -16,11 +17,11 @@ for (int i = 0; i < LENGTH; i++)
 
 	if (arr[i] > maxNumber)
 	{
-		secondMxNumber = maxNumber;
+		secondMaxNumber = maxNumber;
 		maxNumber = arr[i];
 	}
-	else if (arr[i] > secondMxNumber && arr[i] != maxNumber)
-		secondMxNumber = arr[i];
+	else if (arr[i] > secondMaxNumber && arr[i] != maxNumber)
+		secondMaxNumber = arr[i];
 }
 
-Console.WriteLine($"\n\nMax number: {maxNumber}\n\nSecond max number: {secondMxNumber}");
+Console.WriteLine($"\n\nMax number: {maxNumber}\n\nSecond max number: {secondMaxNumber}");
